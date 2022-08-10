565e7b2ed0591126908c3b89e42167bc6578299c
FelipeAdr05 <felipeadrnunes@gmail.com>
2022-08-10 18:42:01 -0300

    Finalização do Bot

--- files/BotIOS.py
+++ files/BotIOS.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-arquivo = open('contactsGoogle.txt', 'r', encoding='utf-8')
+arquivo = open('contactsIOS.txt', 'r', encoding='utf-8')
 lista = []
 
 for i in arquivo.readlines():
     if i[0] == 'T':
-        lista.append(i.rstrip('\n')[14:])
+        lista.append(i.rstrip('\n')[35:])
 
 df = pd.DataFrame(lista)
 
 df = df.rename({0: 'Telefones'}, axis = 1)
 
-df.to_excel('PlanilhatelefonesAndroid.xlsx')
+df.to_excel('PlanilhatelefonesIOS.xlsx')
